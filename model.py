3b6dc117703c850d73e28c20994e2770d1ea7b4a
ChiChaChai <chatchaishaetan@gmail.com>
2019-11-14 16:07:53 +0700

    Add training

--- model.py
+++ model.py
@@ -1,14 +1,22 @@
 # %%
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision.datasets import MNIST
+import torchvision.transforms as transforms
 from torchsummary import summary
-import numpy as np
+from torchvision.datasets import MNIST
 from tqdm import tqdm, trange
+
 # %%
-mnist = MNIST('dataset', download=True)
+
+mnist = MNIST('dataset',
+              download=True,
+              transform=transforms.Compose([
+                  transforms.ToTensor()
+              ])
+              )
 
 # %%
 
@@ -40,19 +48,30 @@ class Net(nn.Module):
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
-        x = F.log_softmax(x)
+        x = F.softmax(x)
+
         return x
 
-    def fit(self, x, y_true, epochs=10,):
+    def fit(self, data_loader, epochs=1,):
+        log_period = 10
         self.optimizer = optim.Adam(self.parameters(), lr=1e-3)
-        for ep in trange(epochs):
-            y_pred = self.forward(x)
-            loss = F.nll_loss(y_pred, y_true)
-            tqdm.write(str(loss.item()))
-            loss.backward()
-            
-            self.optimizer.step()
-            
+        for ep in range(epochs):
+            running_loss = 0.0
+            results = []
+            for minibatch_index, (x, y_true) in enumerate(tqdm(data_loader)):
+                self.optimizer.zero_grad()
+                y_pred = self.forward(x)
+                loss = F.cross_entropy(y_pred, y_true)
+                loss.backward()
+                self.optimizer.step()
+
+                results.extend(
+                    list((torch.max(y_pred, 1)[1] == y_true).numpy()))
+                running_loss += loss.item()
+                if minibatch_index % log_period == 0:
+                    tqdm.write(
+                        f'loss: {running_loss / log_period} acc: {np.asarray(results).mean()}')
+                    running_loss = 0.0
 
 
 # %%
@@ -60,10 +79,12 @@ net = Net()
 summary(net, (1, 28, 28))
 
 # %%
-x = mnist.train_data[:1000].unsqueeze(1).float()
-y = mnist.train_labels[:1000]
+train_loader = torch.utils.data.DataLoader(mnist,
+                                           batch_size=32,
+                                           shuffle=True,)
 # %%
 
-net.fit(x, y)
+net.fit(train_loader, epochs=10)
+
 
 # %%
